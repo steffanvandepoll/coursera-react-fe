45b1bef4ecb665ebdf6fd2833d61df34a0e90408
Steffan van de Poll <steffanvandepoll@gmail.com>
2020-04-08 14:07:09 +0200

    Components Part 1

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 
 
 function App() {
@@ -12,6 +13,9 @@ function App() {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+
+        <Menu />
+
       </div>
   );
 }
